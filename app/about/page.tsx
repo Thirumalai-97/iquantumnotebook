e75c9f4b6efc8d705d8b5efea617a18e04f23d56
Thriumalai-97 <thirumalai11049761@gmail.com>
2025-11-25 18:11:00 +0530

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,17 +5,17 @@ export default function AboutPage() {
 
       <p className="text-gray-300 text-lg leading-relaxed">
         I’m <span className="text-cyan-400 font-semibold">Thirumalai</span>,  
-        a Quantum Engineer working in   
-        <span className="text-violet-400"> Quantum Algorithms </span>,
-        <span className="text-cyan-400"> Research </span>  
-        and <span className="text-violet-400"> and Development</span>.
+        a Quantum Engineer working in
+        <span className="text-violet-400"> Quantum Algorithms </span>
+        <span className="text-cyan-400"> Research and Development </span>  
+        
       </p>
 
       <p className="text-gray-400 leading-relaxed">
         My work focuses on variational quantum algorithms, optimization,
         quantum ML, and building practical quantum applications.  
         I write to document my journey, share insights, and explore the
-        deeper patterns behind technology and life.
+        deeper patterns behind Quantum Technology.
       </p>
 
       <p className="text-gray-400 leading-relaxed">
@@ -26,8 +26,8 @@ export default function AboutPage() {
       <h2 className="text-2xl text-cyan-400 font-semibold mt-10">Connect</h2>
       <ul className="text-gray-300 space-y-2">
         <li>📌 Portfolio — <a className="text-cyan-400" href="https://thirumalai-97.github.io/portfolio" target="_blank">Visit Portfolio</a></li>
-        <li>💼 LinkedIn — <a className="text-cyan-400" href="#" target="_blank">Your LinkedIn</a></li>
-        <li>📧 Email — your.email@example.com</li>
+        <li>💼 LinkedIn — <a className="text-cyan-400" href="https://www.linkedin.com/in/m-thirumalai/" target="_blank">LinkedIn</a></li>
+        <li>📧 Email — er.mthirumalai@gmail.com</li>
       </ul>
     </section>
   );
